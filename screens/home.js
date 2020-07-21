3f5870daf9b975bf201ec8296ae797d1ba9b8eaf
Mritunjay Pathak <cybermritunjay@gmail.com>
2020-07-21 15:32:06 +0530

    react-native-navigation

---
+++ screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+
+    return (
+        <View style={{ margin: 16 }}>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home
